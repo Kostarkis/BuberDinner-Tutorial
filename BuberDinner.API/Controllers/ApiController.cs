87647f763c3626303a2677179f564c36f62dbe90
Kostarkis <bartoszkostarczyk1@gmail.com>
2022-10-27 16:44:26 +0200

    Validation Behavior Part 8

--- BuberDinner.API/Controllers/ApiController.cs
+++ BuberDinner.API/Controllers/ApiController.cs
@@ -2,6 +2,7 @@
 using ErrorOr;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BuberDinner.API.Controllers
 {
@@ -11,10 +12,24 @@ namespace BuberDinner.API.Controllers
     {
         protected IActionResult Problem(List<Error> errors)
         {
+            if (errors.Count is 0)
+            {
+                return Problem();
+            }
+            
+            if (errors.All(error => error.Type == ErrorType.Validation))
+            {
+                return ValidationProblem(errors);
+            }
+            
             HttpContext.Items[HttpContextItemKeys.Errors] = errors;
-            var firstError = errors[0];
+            
+            return Problem(errors[0]);
+        }
 
-            var statusCode = firstError.Type switch
+        private IActionResult Problem(Error error)
+        {
+            var statusCode = error.Type switch
             {
                 ErrorType.Conflict => StatusCodes.Status409Conflict,
                 ErrorType.Validation => StatusCodes.Status400BadRequest,
@@ -22,7 +37,19 @@ namespace BuberDinner.API.Controllers
                 _ => StatusCodes.Status500InternalServerError
             };
 
-            return Problem(statusCode: statusCode, title: firstError.Description);
+            return Problem(statusCode: statusCode, title: error.Description);
+        }
+
+        private IActionResult ValidationProblem(List<Error> errors)
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+            foreach (var error in errors)
+            {
+                modelStateDictionary.AddModelError(
+                    error.Code,
+                    error.Description);
+            }
+            return ValidationProblem(modelStateDictionary);
         }
     }
 }
--- BuberDinner.Application/D_I.cs
+++ BuberDinner.Application/D_I.cs
@@ -1,5 +1,11 @@
-﻿using MediatR;
+﻿using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Authentication.Common.Behaviors;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace BuberDinner.Application;
 
@@ -8,6 +14,12 @@ public static class D_I
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(typeof(D_I).Assembly);
+
+        services.AddScoped(
+            typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehavior<,>));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services;
     }
 }
